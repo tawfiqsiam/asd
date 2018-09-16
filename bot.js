aac0d58366d40b4ffc44bbd4c9cac78395b8d275
plorman <43245138+plorman@users.noreply.github.com>
2018-09-16 21:45:12 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const moment = require('moment');
+
+client.on('ready', () => {
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+});
+
+
+client.on("guildMemberAdd", member => {
+let welcomer = member.guild.channels.find("name","night");
+      if(!welcomer) return;
+      if(welcomer) {
+         moment.locale('ar-ly');
+         var h = member.user;
+        let norelden = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(h.avatarURL)
+        .setAuthor(h.username,h.avatarURL)
+        .addField('`The date you entered DISCORD`',`${moment(member.user.createdAt).format('D/M/YYYY h:mm a')} **\n** \`${moment(member.user.createdAt).fromNow()}\``,true)
+         .setFooter(`${h.tag}`,"https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif")
+     welcomer.send({embed:norelden});          
+               
+ 
+      }
+      });
+	
+
+client.login(process.env.BOT_TOKEN);
